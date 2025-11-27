807dca6ec12806da1c203888d7e0f555380b26a4
AnumZainab999 <anumzainab999@gmail.com>
2025-11-27 15:24:33 +0500

    receive email

--- controllers/insuranceController.js
+++ controllers/insuranceController.js
@@ -1,5 +1,6 @@
 import InsuranceCompany from "../models/InsuranceCompany.js";
 import InsuranceQuote from "../models/InsuranceQuote.js";
+import { sendEmail } from "../utils/email.js";
 
 export const calculateInsurance = async (req, res) => {
   try {
@@ -8,7 +9,7 @@ export const calculateInsurance = async (req, res) => {
     // 1. Fetch all companies
     const companies = await InsuranceCompany.find();
 
-    // 2. Calculate for each company
+    // 2. Calculate for each company (still saving in DB)
     const results = companies.map(company => {
       const insuranceAmount = (value * company.baseRate) / 100;
       const trackerAmount = tracker ? 15000 : 0;
@@ -17,15 +18,13 @@ export const calculateInsurance = async (req, res) => {
       return {
         companyId: company._id,
         companyName: company.name,
-        companyLogo: company.logo, 
-        companyWorkshops: company.workshops,  
         insuranceAmount: Math.round(insuranceAmount),
         trackerAmount: Math.round(trackerAmount),
         total: Math.round(total)
       };
     });
 
-    // 3. Save in database
+    // 3. Save quote in DB
     const newQuote = await InsuranceQuote.create({
       name,
       phoneNumber,
@@ -35,15 +34,27 @@ export const calculateInsurance = async (req, res) => {
       results
     });
 
+    // ----------------------------------------------
+    // 4. Send Email (only request details)
+    // ----------------------------------------------
+    const htmlBody = `
+      <h2>New Insurance Quote Request</h2>
+      <p><b>Name:</b> ${name}</p>
+      <p><b>Phone:</b> ${phoneNumber}</p>
+      <p><b>Car Brand:</b> ${brand}</p>
+      <p><b>Car Value:</b> PKR ${value}</p>
+      <p><b>Tracker Selected:</b> ${tracker ? "Yes" : "No"}</p>
+      <p><b>Quote ID:</b> ${newQuote._id}</p>
+    `;
+
+    // Call email util
+    await sendEmail("New Insurance Quote Received", htmlBody);
+
     res.json({
       success: true,
-      name,
-      phoneNumber,
-      brand,
-      value,
-      trackerApplied: tracker,
-      results,
-      savedQuoteId: newQuote._id
+      message: "Quote calculated & email sent successfully",
+      savedQuoteId: newQuote._id,
+      results
     });
 
   } catch (error) {
